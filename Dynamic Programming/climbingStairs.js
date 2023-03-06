f8e28e0cbac395262f0b14ee73cbd4641da052e1
Zhaowei <sunzhaow0813@gmail.com>
2023-03-06 08:41:38 -0500

    Add iterative solution to climb stairs

--- Dynamic Programming/climbingStairs.js
+++ Dynamic Programming/climbingStairs.js
@@ -21,21 +21,30 @@ Explanation: There are three ways to climb to the top.
 
 const climbStairs = (n, memo={}) => {
     //RECURSIVE
-    //base case
-    if (n <= 1) return 1;
-    //check memo
-    let oneStairDown, twoStairsDown
-    if (memo[n-1] !== undefined) oneStairDown = memo[n-1];
-    else {
-        memo[n-1] = climbStairs(n-1, memo);
-        oneStairDown = memo[n-1]
-    };
+    // //base case
+    // if (n <= 1) return 1;
+    // //check memo
+    // let oneStairDown, twoStairsDown
+    // if (memo[n-1] !== undefined) oneStairDown = memo[n-1];
+    // else {
+    //     memo[n-1] = climbStairs(n-1, memo);
+    //     oneStairDown = memo[n-1]
+    // };
 
-    if (memo[n-2] !== undefined) twoStairsDown = memo[n-2];
-    else {
-        memo[n-2] = climbStairs(n-2, memo);
-        twoStairsDown = memo[n-2]
-    };
-    //recursive call
-    return oneStairDown + twoStairsDown;
+    // if (memo[n-2] !== undefined) twoStairsDown = memo[n-2];
+    // else {
+    //     memo[n-2] = climbStairs(n-2, memo);
+    //     twoStairsDown = memo[n-2]
+    // };
+    // //recursive call
+    // return oneStairDown + twoStairsDown;
+
+    //ITERATIVE
+    const arr = [];
+    arr[1] = 1;
+    arr[2] = 2;
+    for(let i = 3; i <= n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
 };
