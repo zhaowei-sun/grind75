eb72b33fefe5892251aed44c42a9c60271d80193
Zhaowei <sunzhaow0813@gmail.com>
2023-03-06 08:20:16 -0500

    Set up ransom note

---
+++ Hash Table/ransomNote.js
@@ -0,0 +1,26 @@
+/*
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+*/
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+
+const canConstruct = (ransomNote, magazine) => {
+    
+};
