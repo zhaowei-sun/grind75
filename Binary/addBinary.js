5b3773768cbb8aada2e67709f85be2fb7d616790
Zhaowei <sunzhaow0813@gmail.com>
2023-03-10 10:32:48 -0500

    Update add binary to use BigInt

--- Binary/addBinary.js
+++ Binary/addBinary.js
@@ -11,20 +11,14 @@ Output: "10101"
 */
 
 const addBinary = function(a, b) {
-    let num1 = 0, num2 = 0;
-    for (let i = 0; i < a.length; i++) {
-        num1 += Number(a[i]) * (2 ** (a.length - 1 - i));
-    }
-    for (let j = 0; j < b.length; j++) {
-        num2 += Number(b[j]) * (2 ** (b.length - 1 - i));
-    }
-    const num = num1 + num2;
-    let result = '', curr = num;
-    while (curr >= 2) {
-        result += String(curr % 2);
-        curr = Math.floor(curr / 2);
-    }
-    result += String(curr);
-    return result;
+    //USING BigInt
+    const aBin = `0b${a}`
+    const bBin = `0b${b}`
+    const sum = BigInt(aBin) + BigInt(bBin)
+    return sum.toString(2)
 };
+
+let a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101"
+let b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+console.log(addBinary(a, b));
 
