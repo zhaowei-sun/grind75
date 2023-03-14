a9b6f81cf21297f71a195210ac6f77c847a7eb6a
Zhaowei <sunzhaow0813@gmail.com>
2023-03-14 09:30:06 -0400

    Add merge two sorted lists additional solution

--- Linked List/mergeTwoSortedLists.js
+++ Linked List/mergeTwoSortedLists.js
@@ -49,4 +49,20 @@ const mergeTwoLists = (list1, list2) => {
     }
 
     return arr[0];
+
+    //SIMPLER SOLUTION
+    // let mergedHead = { val : -1, next : null }, crt = mergedHead;
+    // while(l1 && l2) {
+    //     if(l1.val > l2.val) {
+    //         crt.next = l2;
+    //         l2 = l2.next;
+    //     } else {
+    //         crt.next = l1;
+    //         l1 = l1.next;
+    //     }
+    //     crt = crt.next;
+    // }
+    // crt.next = l1 || l2;
+    
+    // return mergedHead.next;
 };
